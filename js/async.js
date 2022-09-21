f99a686cc33eb2e9cc1c0fbc7bb82e2fc5cc10c9
jonathan-crepeau <jon.crepeau@gmail.com>
2022-09-21 14:37:46 -0700

    Example 1: Complete An HTTP Request w/ XMLHttpRequest Object complete

--- js/async.js
+++ js/async.js
@@ -24,3 +24,51 @@
 // NOTE - In this next call, we'll intentionally change the url to include a pokemon ID that is incorrect ('77a'). Again, the Fetch API differs from jQuery.ajax() because the original Promise will still resolve and return us a Response object. However, we can see the differences compared to the above call with the property values now at (status: 404) and (ok:false):
 // fetch("https://pokeapi.co/api/v2/pokemon/77a/")
 //     .then((response) => console.log(response));
+
+
+
+// ANCHOR Using jQuery.ajax()
+
+// SECTION Example 1: Getting JSON text from jQuery.ajax() response:
+
+// NOTE - Returns JSON body text:
+// $.ajax({
+//     method: "GET",
+//     url: "https://pokeapi.co/api/v2/pokemon/77",
+//     success: function(result){
+//         console.log(result);
+//     }
+// });
+
+
+// ANCHOR Completing an XMLHttpRequest
+
+// SECTION Example 1: Completing An HTTP Request w/ XMLHttpRequest object:
+document.querySelector('#ajax-button').addEventListener('click', makeRequest);
+
+let httpRequest;
+
+function makeRequest() {
+    // NOTE - calling a new XMLHttpRequest object:
+    httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        console.log('Giving Up: Cannot create an XMLHTTP instance.')
+        return false;
+    }
+    // NOTE - Set what function to call upon state change:
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open("GET", "https://pokeapi.co/api/v2/pokemon/77/");
+    httpRequest.send();
+}
+
+function alertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            // NOTE - In the first try, I didn't parse the return as JSON and got a mess of what looked ot be just text or html text. Notice that AJAX, which is a jQuery core method, has a different syntax (JSON.parse()) than its parallel in the Fetch API ((response) => response.json()):
+            const response = JSON.parse(httpRequest.responseText);
+            console.log(response);
+        } else {
+            alert("Problem with request, status is something other than 200.");
+        }
+    }
+}
