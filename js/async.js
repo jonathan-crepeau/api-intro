d267946fe6af37c55e6e87ba4b6f71f1d4d9293c
jonathan-crepeau <jon.crepeau@gmail.com>
2022-09-21 14:57:37 -0700

    Example 2: Log name of Pokemon from HTTP GET Request to Pokemon API using callback functiion complete

--- js/async.js
+++ js/async.js
@@ -41,34 +41,81 @@
 // });
 
 
+// SECTION Example 2: Logging name of Pokemon from HTTP GET Request via callback function:
+let pokemonNames = [];
+
+function makeGetRequest(method, callback){
+    $.ajax({
+        method: `${method}`,
+        url: "https://pokeapi.co/api/v2/pokemon/77",
+        success: function(result){
+            callback(result.name);
+        }
+    })
+}
+
+function getPokemonName(input){
+    console.log(input);
+}
+
+makeGetRequest("GET", getPokemonName);
+
+
 // ANCHOR Completing an XMLHttpRequest
 
 // SECTION Example 1: Completing An HTTP Request w/ XMLHttpRequest object:
-document.querySelector('#ajax-button').addEventListener('click', makeRequest);
+// document.querySelector('#ajax-button').addEventListener('click', makeRequest);
 
-let httpRequest;
 
-function makeRequest() {
-    // NOTE - calling a new XMLHttpRequest object:
-    httpRequest = new XMLHttpRequest();
-    if (!httpRequest) {
-        console.log('Giving Up: Cannot create an XMLHTTP instance.')
-        return false;
-    }
-    // NOTE - Set what function to call upon state change:
-    httpRequest.onreadystatechange = alertContents;
-    httpRequest.open("GET", "https://pokeapi.co/api/v2/pokemon/77/");
-    httpRequest.send();
-}
+// let httpRequest;    
+// function makeRequest() {
+//     // NOTE - calling a new XMLHttpRequest object:
+//     httpRequest = new XMLHttpRequest();
+//     if (!httpRequest) {
+//         console.log('Giving Up: Cannot create an XMLHTTP instance.')
+//         return false;
+//     }
+//     // NOTE - Set what function to call upon state change:
+//     httpRequest.onreadystatechange = alertContents;
+//     httpRequest.open("GET", "https://pokeapi.co/api/v2/pokemon/77/");
+//     httpRequest.send();
+// }
 
-function alertContents() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        if (httpRequest.status === 200) {
-            // NOTE - In the first try, I didn't parse the return as JSON and got a mess of what looked ot be just text or html text. Notice that AJAX, which is a jQuery core method, has a different syntax (JSON.parse()) than its parallel in the Fetch API ((response) => response.json()):
-            const response = JSON.parse(httpRequest.responseText);
-            console.log(response);
-        } else {
-            alert("Problem with request, status is something other than 200.");
-        }
-    }
-}
+// function alertContents() {
+//     if (httpRequest.readyState === XMLHttpRequest.DONE) {
+//         if (httpRequest.status === 200) {
+//             // NOTE - In the first try, I didn't parse the return as JSON my returned server response was a string of text. Notice that AJAX, which is a jQuery core method, has a different syntax (JSON.parse()) than its parallel in the Fetch API ((response) => response.json()):
+//             const response = JSON.parse(httpRequest.responseText);
+//             console.log(response);
+//         } else {
+//             alert("Problem with request, status is something other than 200.");
+//         }
+//     }
+// }
+
+// function addOne(init, callback) {
+//     const result = init + 1;
+//     callback(result);
+// }
+
+// function addTwo(init, callback){
+//     const result = init + 2;
+//     callback(result);
+// }
+
+// function addThree(init, callback){
+//     const result = init + 3;
+//     callback(result);
+// }
+
+// function performOperation(){
+//     addOne(6, (result1) => {
+//         addTwo(result1, (result2) => {
+//             addThree(result2, (result3) =>{
+//                 console.log(`result: ${result3}`);
+//             })
+//         })
+//     })
+// }
+
+// performOperation();
